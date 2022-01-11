df6aeb59b02e4984c49e4a90bfc3bd62da80dc51
dominici94 <mattiadominici94@gmail.com>
2022-01-11 13:23:16 +0100

    init progetto

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <Header/>
     <Main/>
     <Footer/>
@@ -9,7 +7,6 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 import Header from './components/macro/Header.vue';
 import Main from './components/macro/Main.vue';
 import Footer from './components/macro/Footer.vue';
@@ -17,7 +14,6 @@ import Footer from './components/macro/Footer.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     Header,
     Main,
     Footer
@@ -26,12 +22,7 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/style/global.scss';
+// @import './assets/style/partials/variables.scss';
+
 </style>
