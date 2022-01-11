d02258deea3e5350c515e4f5250f15ce6e54b2ac
dominici94 <mattiadominici94@gmail.com>
2022-01-11 12:39:20 +0100

    img + macro components

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,25 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Main/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/macro/Header.vue';
+import Main from './components/macro/Main.vue';
+import Footer from './components/macro/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header,
+    Main,
+    Footer
   }
 }
 </script>
