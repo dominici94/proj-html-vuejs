124571470df6c2af9482d71f499f983a3d5aa2f5
dominici94 <mattiadominici94@gmail.com>
2022-01-11 16:16:11 +0100

    import fontawesome e googlefont

--- src/components/macro/Header.vue
+++ src/components/macro/Header.vue
@@ -1,6 +1,24 @@
 <template>
   <header>
-      
+      <div class="container">
+          <div class="top-header">
+            <div class="info">
+
+              <ul>
+                <li><a href="#">Download App <span>ios</span></a></li>
+                <li><a href="#">Advertise with us</a></li>
+              </ul>
+            </div>
+
+            <ul class="contact">
+              <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+              <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+            </ul>
+
+          </div>
+      </div>
   </header>
 </template>
 
@@ -10,6 +28,30 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
+@import '../../assets/style/partials/variables.scss';
+
+.top-header{
+  display: flex;
+  justify-content: space-between;
+
+  ul{
+    list-style: none;
+    display: flex;
+
+    a{
+      text-decoration: none;
+      color: $contactColor;
+
+      span{
+        color: white;
+        background-color: $btnColor;
+        padding: 5px;
+        border-radius: 5px;
+      }
+    }
+  }
+}
+
 
 </style>
