2471808b1d258a96be404945ab65a3361dfd8353
dominici94 <mattiadominici94@gmail.com>
2022-01-13 01:51:36 +0100

    section blog

--- src/components/sections/Blog.vue
+++ src/components/sections/Blog.vue
@@ -143,6 +143,71 @@
                     <li><a href="#"><i class="fab fa-youtube"></i></a></li>
                     <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
                 </ul>
+
+                <button class="active">Popular</button>
+                <button>Recent</button>
+
+                <div class="posts">
+
+                    <div class="post">
+                        <img src="../../assets/img/single-post-img3-150x150.jpg" alt="sushi">
+                        <div class="text">
+                            <div class="title">Food Corner: Top Japanese Restaurants for Sushi</div>
+                            <div class="subtitle">March 25th, 2019</div>
+                        </div>
+                    </div>
+
+                    <div class="post">
+                        <img src="../../assets/img/singapore-featured-image-150x150.jpg" alt="sushi">
+                        <div class="text">
+                            <div class="title">City Guide: Singapore</div>
+                            <div class="subtitle">February 27th, 2019</div>
+                        </div>
+                    </div>
+
+                    <div class="post">
+                        <img src="../../assets/img/Mixed-fruits.jpg" alt="sushi">
+                        <div class="text">
+                            <div class="title">6 Nutritional Tips to Help Burn Body Fat</div>
+                            <div class="subtitle">February 28th, 2019</div>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div class="tweet">
+                    <div class="top">
+                        <div class="text">
+                            Tweets by<span>@Theme_Fusion</span>
+                        </div>
+                        <img class="icon" src="../../assets/img/image.svg" alt="info">
+                    </div>
+
+                    <div class="main-post">
+                        <div class="left">
+                            <img class="icon" src="../../assets/img/logo_normal.png" alt="logo">
+                        </div>
+
+                        <div class="right">
+                            <div class="top-right">
+                                <div class="user">Theme Fusion</div>
+                                <img class="icon" src="../../assets/img/image (1).svg" alt="twitter">
+                            </div>
+                            <h5>@Theme_Fusion</h5>
+                            <p>Do you need sublime Wordpress hosting fot your next website? Take advantage of exclusive partner offers that we have secured just for you, & launch your site in seconds with <span class="tag">#avada</span> on WP Engine hosting & get 30% Off this Black Friday <span class="tag"> bit.ly/3kjLLe2 #BlackFriday2020</span></p>
+                            <img id="post" src="../../assets/img/wp.jpeg" alt="wordpress">
+                            <div class="bottom">
+                                <div class="icons">
+                                    <img class="icon" src="../../assets/img/image (2).svg" alt="like">
+                                    <img class="icon" src="../../assets/img/image (3).svg" alt="share">
+                                </div>
+                                <div class="time">21<span>h</span></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
 
         </div>
@@ -194,7 +259,7 @@ export default {
 
         &__subtitle{
             color: $contactColor;
-            width: 40%;
+            width: 60%;
             line-height:30px
         }
     }
@@ -347,7 +412,7 @@ export default {
                 }
 
                 .city{
-                    font-size: 28px;
+                    font-size: 14px;
                     padding: 10px;
                     position: absolute;
                     top: 50%;
@@ -397,6 +462,119 @@ export default {
                 }
             }
 
+            button{
+                font-family: 'Orelega One', cursive;
+                margin: 40px 0;
+                width: 50%;
+                background-color: $mainBgColor;
+                border: 1px solid $secondBtnColor;
+                cursor: pointer;
+                padding: 10px 0;
+            }
+
+            .active{
+                background-color: #fff;
+            }
+            
+            .posts{
+                margin: 10px 0;
+
+                .post{
+                    display: flex;
+                    margin-bottom: 20px;
+
+                    img{
+                        object-fit: cover;
+                        width: 20%;
+                        border-radius: 50%;
+                        margin-right: 10px;
+                        padding: 5px 0;
+                    }
+
+                    .title{
+                        line-height: 20px;
+                    }
+
+                    .subtitle{
+                        color: $contactColor;
+                        font-size: 12px;
+                        line-height: 20px;
+                    }
+                }
+            }
+
+            .tweet{
+                background-color: #fff;
+                border-radius: 5px;
+                padding: 10px;
+
+                .tag{
+                    color: $tagColor;
+                    font-size: 12px;
+                }
+
+                .top{
+                    display: flex;
+                    justify-content: space-between;
+                    padding: 10px 0;
+                }
+
+                .icon{
+                    height: 18px;
+                }
+
+                .main-post{
+                    display: flex;
+                    justify-content: space-between;
+
+                    .left{
+                        width: 15%;
+
+                        img{
+                            width: 100%;
+                            object-fit: contain;                        
+                        }
+                    }
+
+                    .right{
+                        width:85%;
+
+                        .top-right{
+                            display: flex;
+                            justify-content: space-between;
+
+                            .user{
+                                font-weight: bold;
+                            }
+                        }
+
+                        h5{
+                            color: $contactColor;
+                        }
+
+                        #post{
+                            width: 100%;
+                            border-radius: 5px;
+                        }
+
+                        .bottom{
+                            display: flex;
+                            justify-content: space-between;
+                            padding: 5px 0;
+                            color: $contactColor;
+
+                            img{
+                                margin-right: 10px;
+                            }
+
+                            span{
+                                border-bottom: 1px dotted;
+                            }
+                        }
+
+                    }
+                }
+            }
         }
     }
     
