5cbfa0a75b865631c9f7323e283b87666a6829ec
dominici94 <mattiadominici94@gmail.com>
2022-01-13 11:33:13 +0100

    top-footer

--- src/components/macro/Footer.vue
+++ src/components/macro/Footer.vue
@@ -1,6 +1,45 @@
 <template>
   <footer>
-      
+      <div class="top-footer">
+        <div class="container">
+
+          <div class="col social">
+            <img src="../../assets/img/avada-food-logo-mob.png" alt="avada logo">
+            <ul>
+              <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+              <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+            </ul>
+          </div>
+
+          <div class="col list">
+            <ul>
+              <li><a href="#">Recipes</a></li>
+              <li><a href="#">Places</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+
+          <div class="col contact">
+            <div class="title">contact</div>
+            <h4 id="address"><span><i class="fas fa-map-marker-alt"></i></span> 775 New York Ave, Brooklyn, Kings, New York 11203</h4>
+            <h4 id="tel"><span><i class="fas fa-phone"></i></span>+0100-505-0000</h4>
+            <h4 id="mail"><span><i class="fas fa-envelope"></i></span>info@your-domain.com</h4>
+          </div>
+
+          <div class="col download">
+            <div class="title">get our app!</div>
+            <a href="#"><img src="../../assets/img/download-android.png" alt="android"></a>
+            <a href="#"><img src="../../assets/img/download-app-store.png" alt="apple"></a>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="bottom-footer"></div>
   </footer>
 </template>
 
@@ -10,6 +49,89 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../../assets/style/partials/variables.scss';
+
+.top-footer{
+  padding: 30px 0;
+}
+
+.container{
+  display: flex;
+
+  .col{
+    width: calc(100% / 4);
+    border-right: 1px dashed $contactColor;
+    padding: 0 20px;
+
+    img{
+      width: 100%;
+    }
+
+  }
+
+  .social{
+
+    ul{
+      list-style: none;
+      display: flex;
+      padding: 20px 0;
+
+      li{
+        margin-right: 20px;
+      }
+
+      a{
+        text-decoration: none;
+        color: $contactColor;
+      }
+    }
+  }
+
+  .list ul{
+    padding: 20px 0;
+    list-style: none;
+
+    a{
+      text-decoration: none;
+      color: inherit;
+      padding-bottom: 10px;
+      display: block;
+    }
+  }
+
+  .title{
+    text-transform: uppercase;
+    color: $btnColor;
+    padding-bottom: 10px;
+    font-weight: bold;
+  }
+
+  .contact{
+    color: $contactColor;
+
+    h4{
+      font-size: 13px;
+      padding: 10px 30px;
+      position: relative;
+
+
+      span{
+        position: absolute;
+        top: 10px;
+        left: 5px;
+      }
+    }
+
+  }
+
+  .download{
+    border: none;
+
+    img{
+      padding: 10px 0;
+    }
+  }
+}
 
 </style>
---
+++ src/components/sections/MealsList.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="meals-list">
+
+      <div class="meal-card">
+          <img src="../../assets/img/Yogurt-Nan.jpg" alt="yogurt">
+      </div>
+
+      <div class="meal-card">
+          <img src="../../assets/img/Mixed-fruits.jpg" alt="fruits">
+      </div>
+
+      <div class="meal-card">
+          <img src="../../assets/img/r-rachel-park-366508-unsplash-min.jpg" alt="breakfast">
+      </div>
+
+      <div class="meal-card">
+          <img src="../../assets/img/r-michelle-tsang-500721-unsplash-min.jpg" alt="icecream">
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'MealList'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/style/partials/variables.scss';
+
+.meals-list{
+    display: flex;
+
+    .meal-card{
+        width: calc(100% / 4);
+
+        img{
+            width: calc(100%);
+        }
+    }
+}
+
+
+
+
+
+</style>
